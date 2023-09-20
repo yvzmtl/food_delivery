ec9bc0199240a3bfc5b5e73fe9a3c328b5306f7e
yavuzmutlu <selim.mutlu@bil.omu.edu.tr>
2023-09-20 16:34:37 +0300

    update

--- lib/data/repository/location_repo.dart
+++ lib/data/repository/location_repo.dart
@@ -1,5 +1,8 @@
 
 import 'package:flutter_food_delivery/data/api/api_client.dart';
+import 'package:flutter_food_delivery/utils/app_constants.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LocationRepo{
@@ -7,6 +10,11 @@ class LocationRepo{
   final SharedPreferences sharedPreferences;
 
   LocationRepo({required this.apiClient, required this.sharedPreferences});
+
+  Future<Response> getAddressfromGeocode(LatLng latLng) async {
+    return await apiClient.getData('${AppConstants.GEOCODE_URI}'
+    '?lat=${latLng.latitude}&lng=${latLng.longitude}');
+  }
 
   
 }
